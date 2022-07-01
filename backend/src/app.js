552b6ca78d331319b5d74366978e343363f3c3a7
froustex <stephane-feroussier_student2022@wilder.school>
2022-07-01 10:01:56 +0200

    commit chat

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const path = require("path");
 
-const cors = require("cors");
+const http = require("http");
 
+const cors = require("cors");
+const { Server } = require("socket.io");
 // let's create express app
 
 const app = express();
+const server = http.createServer(app);
 
 // use some application-level middlewares
 app.use(
@@ -15,6 +18,31 @@ app.use(
     credentials: true,
   })
 );
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.warn(`Utilisateur connecté ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.warn(`Utilisateur avec ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+    console.warn(data);
+  });
+
+  socket.on("disconnect", () => {
+    console.warn("user disconnected", socket.id);
+  });
+});
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
@@ -26,4 +54,4 @@ const router = require("./router");
 app.use(router);
 
 // ready to export
-module.exports = app;
+module.exports = server;
